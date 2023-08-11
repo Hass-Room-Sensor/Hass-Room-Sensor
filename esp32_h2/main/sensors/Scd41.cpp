01096028169ea24d8a3f466fcd662149335d41e4
Fabian Sauter <sauter.fabian@mailbox.org>
2023-08-11 21:49:25 +0200

    SCD41 properly setting the altitude and obtaining config

--- esp32_h2/main/sensors/Scd41.cpp
+++ esp32_h2/main/sensors/Scd41.cpp
@@ -5,12 +5,13 @@
 #include <array>
 #include <cassert>
 #include <chrono>
+#include <cmath>
 #include <optional>
 #include <span>
 #include <stdexcept>
+#include <sys/_stdint.h>
 #include <thread>
 #include <vector>
-#include <sys/_stdint.h>
 
 namespace sensors {
 const char* Scd41::TAG = "SCD41";
@@ -56,6 +57,14 @@ bool Scd41::init() const {
         return false;
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    // For Dagersheim: https://de-de.topographic-map.com/map-27vsrr/Dagersheim/
+    set_sensor_altitude(438);
+    // persist_settings(); // "The EEPROM is guaranteed to withstand at least 2000 write cycles."
+
+    ESP_LOGI(TAG, "SCD41 serial number: %012llx", get_serial_number());
+    ESP_LOGI(TAG, "SCD41 altitude: %u", get_sensor_altitude());
+    ESP_LOGI(TAG, "SCD41 temperature offset: %f", get_temperature_offset());
 
     if (!start_periodic_measurement()) {
         return false;
@@ -127,6 +136,33 @@ bool Scd41::write(uint16_t reg, std::chrono::milliseconds timeout) const {
     return true;
 }
 
+bool Scd41::write(uint16_t reg, uint16_t payload, std::chrono::milliseconds timeout) const {
+    std::array<uint8_t, 2> data{static_cast<uint8_t>(reg >> 8), static_cast<uint8_t>(reg & 0xFF)};
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+
+    i2c_master_write_byte(cmd, (DEVICE_ADDR << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write(cmd, data.data(), data.size(), true);
+
+    // Payload
+    std::vector<uint8_t> payloadBytes{0x0, 0x0};
+    uint8_t crc = calc_crc(payloadBytes);
+    payloadBytes.push_back(crc);
+    i2c_master_write(cmd, payloadBytes.data(), payloadBytes.size(), true);
+
+    i2c_master_stop(cmd);
+
+    esp_err_t result = i2c_master_cmd_begin(port, cmd, static_cast<TickType_t>(timeout.count()) / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to write for write command.");
+        return false;
+    }
+    return true;
+}
+
 bool Scd41::write_read(uint16_t reg, std::span<uint16_t> response, std::chrono::milliseconds timeout) const {
     std::array<uint8_t, 2> data{static_cast<uint8_t>(reg >> 8), static_cast<uint8_t>(reg & 0xFF)};
 
@@ -176,36 +212,39 @@ bool Scd41::write_read(uint16_t reg, std::span<uint16_t> response, std::chrono::
 }
 
 bool Scd41::start_periodic_measurement() const {
-    ESP_LOGI(TAG, "Starting measurement...");
+    ESP_LOGD(TAG, "Starting measurement...");
     if (write(0x21b1, std::chrono::milliseconds(10))) {
-        ESP_LOGI(TAG, "Measurement started.");
+        ESP_LOGD(TAG, "Measurement started.");
         return true;
     } else {
         ESP_LOGE(TAG, "Started measurement failed.");
-        return true;
+        return false;
     }
 }
 
 bool Scd41::stop_periodic_measurement() const {
-    ESP_LOGI(TAG, "Stopping measurement...");
+    ESP_LOGD(TAG, "Stopping measurement...");
     if (write(0x3f86, std::chrono::milliseconds(500))) {
-        ESP_LOGI(TAG, "Measurement stopped.");
+        ESP_LOGD(TAG, "Measurement stopped.");
         return true;
     } else {
         ESP_LOGE(TAG, "Stopping measurement failed.");
-        return true;
+        return false;
     }
 }
 
 std::optional<measurement_t> Scd41::read_measurement() const {
+    ESP_LOGD(TAG, "Reading measurement...");
     std::array<uint16_t, 3> data{};
     if (!write_read(0xec05, data, std::chrono::milliseconds(10))) {
+        ESP_LOGE(TAG, "Failed to read measurement.");
         return std::nullopt;
     }
     return convert_measurement(data);
 }
 
 bool Scd41::get_data_ready_status() const {
+    ESP_LOGD(TAG, "Getting data ready state...");
     std::array<uint16_t, 1> response{};
     if (!write_read(0xe4b8, response, std::chrono::milliseconds(1))) {
         ESP_LOGE(TAG, "Failed to check if device is ready.");
@@ -217,28 +256,71 @@ bool Scd41::get_data_ready_status() const {
 }
 
 bool Scd41::perform_self_test() const {
-    ESP_LOGI(TAG, "Starting self test...");
+    ESP_LOGD(TAG, "Starting self test...");
     std::array<uint16_t, 1> response{};
     if (!write_read(0x3639, response, std::chrono::milliseconds(10000))) {
-        ESP_LOGI(TAG, "Failed to perform self test.");
+        ESP_LOGE(TAG, "Failed to perform self test.");
         return false;
     }
     return response[0] == 0;
 }
 
 uint64_t Scd41::get_serial_number() const {
-    ESP_LOGI(TAG, "Requesting serial number...");
+    ESP_LOGD(TAG, "Requesting serial number...");
     std::array<uint16_t, 3> response{};
-    if (!write_read(0x3682, response, std::chrono::milliseconds(10))) {
-        ESP_LOGI(TAG, "Failed to get serial number.");
+    if (!write_read(0x3682, response, std::chrono::milliseconds(1))) {
+        ESP_LOGE(TAG, "Failed to get serial number.");
         return false;
     }
 
-    uint64_t result = (static_cast<uint64_t>(response[0]) << 32) | (static_cast<uint64_t>(response[1]) << 16) | static_cast<uint64_t>(response[2]);
-    ESP_LOGI(TAG, "Serial number: %012llx", result);
-    return result;
+    return (static_cast<uint64_t>(response[0]) << 32) | (static_cast<uint64_t>(response[1]) << 16) | static_cast<uint64_t>(response[2]);
 }
 
+void Scd41::set_temperature_offset(double offset) const {
+    ESP_LOGD(TAG, "Setting temperature offset...");
+    uint16_t temp = static_cast<uint16_t>(std::round(offset * (static_cast<double>(0xFFFF) / 175.0)));
+    if (!write(0x241d, temp, std::chrono::milliseconds(1))) {
+        ESP_LOGE(TAG, "Failed to perform write_read inside set_temperature_offset.");
+        return;
+    }
+}
+
+double Scd41::get_temperature_offset() const {
+    ESP_LOGD(TAG, "Requesting temperature offset...");
+    std::array<uint16_t, 1> data{};
+    if (!write_read(0x2318, data, std::chrono::milliseconds(1))) {
+        ESP_LOGE(TAG, "Failed to perform write_read inside get_temperature_offset.");
+        return -1;
+    }
+    return static_cast<double>(data[0]) * (175.0 / static_cast<double>(0xFFFF));
+}
+
+void Scd41::set_sensor_altitude(uint16_t altitude) const {
+    ESP_LOGD(TAG, "Setting altitude...");
+    if (!write(0x2427, altitude, std::chrono::milliseconds(1))) {
+        ESP_LOGE(TAG, "Failed to perform write_read inside set_sensor_altitude.");
+        return;
+    }
+}
+
+uint16_t Scd41::get_sensor_altitude() const {
+    ESP_LOGD(TAG, "Requesting altitude...");
+    std::array<uint16_t, 1> data{};
+    if (!write_read(0x2322, data, std::chrono::milliseconds(1))) {
+        ESP_LOGE(TAG, "Failed to perform write_read inside get_sensor_altitude.");
+        return -1;
+    }
+    return data[0];
+}
+
+void Scd41::persist_settings() const {
+    ESP_LOGD(TAG, "Storing settings persistent...");
+    if (write(0x3615, std::chrono::milliseconds(800))) {
+        ESP_LOGD(TAG, "Settings stored persistent.");
+    } else {
+        ESP_LOGE(TAG, "Storing settings persistent failed.");
+    }
+}
 uint8_t Scd41::calc_crc(const std::span<uint8_t> data) {
     static const uint8_t CRC8_POLYNOMIAL = 0x31;
     static const uint8_t CRC8_INIT = 0xFF;
@@ -259,10 +341,23 @@ uint8_t Scd41::calc_crc(const std::span<uint8_t> data) {
 }
 
 measurement_t Scd41::convert_measurement(std::span<uint16_t, 3> data) {
-    uint16_t co2 = data[0];
-    double temp = -45.0 + 175.0 * static_cast<double>(data[1]) / static_cast<double>((2 << 16) - 1);
-    double hum = 100 * static_cast<double>(data[2]) / static_cast<double>((2 << 16) - 1);
+    uint16_t co2 = convert_measurement_to_co2(data[0]);
+    double temp = convert_measurement_to_temp(data[1]);
+    double hum = convert_measurement_to_hum(data[2]);
 
     return {co2, temp, hum};
 }
-}  // namespace sensors
+
+double Scd41::convert_measurement_to_temp(uint16_t mTemp) {
+    return -45.0 + 175.0 * (static_cast<double>(mTemp) / static_cast<double>(0xFFFF));
+}
+
+double Scd41::convert_measurement_to_hum(uint16_t mHum) {
+    return 100 * static_cast<double>(mHum) / static_cast<double>((2 << 16) - 1);
+}
+
+uint16_t Scd41::convert_measurement_to_co2(uint16_t mCo2) {
+    return mCo2; // Nothing needs to be done here
+}
+
+} // namespace sensors
--- esp32_h2/main/sensors/Scd41.hpp
+++ esp32_h2/main/sensors/Scd41.hpp
@@ -22,7 +22,7 @@ struct measurement_t {
  * Specification: https://sensirion.com/media/documents/48C4B7FB/6426E14D/CD_DS_SCD40_SCD41_Datasheet_D1_052023.pdf
  **/
 class Scd41 {
- private:
+  private:
     static constexpr uint8_t DEVICE_ADDR = 0x62;
     static const char* TAG;
 
@@ -32,7 +32,7 @@ class Scd41 {
 
     std::array<uint8_t, 2> buffer;
 
- public:
+  public:
     Scd41(i2c_port_t port, gpio_num_t sda, gpio_num_t scl);
     Scd41(Scd41&&) = default;
     Scd41(const Scd41&) = default;
@@ -48,16 +48,25 @@ class Scd41 {
     [[nodiscard]] bool stop_periodic_measurement() const;
     [[nodiscard]] bool perform_self_test() const;
     [[nodiscard]] uint64_t get_serial_number() const;
+    void set_temperature_offset(double offset) const;
+    [[nodiscard]] double get_temperature_offset() const;
+    void set_sensor_altitude(uint16_t altitude) const;
+    [[nodiscard]] uint16_t get_sensor_altitude() const;
+    void persist_settings() const;
 
- private:
+  private:
     static uint8_t calc_crc(const std::span<uint8_t> data);
 
     static void transform_to_send_data(const std::span<uint16_t> data, std::span<uint8_t> dataPrepared);
     static bool validate_transform_received_data(const std::span<uint8_t> dataReceived, std::span<uint16_t> data);
 
     [[nodiscard]] bool write(uint16_t reg, std::chrono::milliseconds timeout) const;
+    [[nodiscard]] bool write(uint16_t reg, uint16_t payload, std::chrono::milliseconds timeout) const;
     [[nodiscard]] bool write_read(uint16_t reg, std::span<uint16_t> response, std::chrono::milliseconds timeout) const;
 
     static measurement_t convert_measurement(std::span<uint16_t, 3> data);
+    static double convert_measurement_to_temp(uint16_t mTemp);
+    static double convert_measurement_to_hum(uint16_t mHum);
+    static uint16_t convert_measurement_to_co2(uint16_t mCo2);
 };
-}  // namespace sensors
+} // namespace sensors
