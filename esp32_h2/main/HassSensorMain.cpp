4f2bf795f529d625e04eb6db966c8a41449cfa79
Fabian Sauter <sauter.fabian@mailbox.org>
2025-01-16 19:27:51 +0100

    Enabled the LED again

--- esp32_h2/main/HassSensorMain.cpp
+++ esp32_h2/main/HassSensorMain.cpp
@@ -24,15 +24,15 @@ void mainLoop() {
     ESP_ERROR_CHECK(nvs_flash_init());
 
     // Initialize the LED:
-    /*std::shared_ptr<actuators::RgbLed> rgbLed = std::make_shared<actuators::RgbLed>(GPIO_NUM_8);
+    std::shared_ptr<actuators::RgbLed> rgbLed = std::make_shared<actuators::RgbLed>(GPIO_NUM_8);
     rgbLed->init();
     rgbLed->on(actuators::color_t{0, 0, 30});
-    zigbee::ZDevice::get_instance()->set_led(rgbLed);*/
+    zigbee::ZDevice::get_instance()->set_led(rgbLed);
 
     // Initialize the SCD41 sensor:
     sensors::Scd41 scd41(GPIO_NUM_12, GPIO_NUM_22);
     if (!scd41.init()) {
-        // rgbLed->on(actuators::color_t{30, 0, 0});
+        rgbLed->on(actuators::color_t{30, 0, 0});
         ESP_LOGE(TAG, "Initializing SCD41 failed. Rebooting...");
         esp_restart();
     }
