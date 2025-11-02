8c81aea9c991df27d23d977c606f5d7cab007537
Fabian Sauter <sauter.fabian@mailbox.org>
2025-11-02 16:39:53 +0100

    New sdkconfig based device selection

--- esp32/main/DeviceDefs.hpp
+++ esp32/main/DeviceDefs.hpp
@@ -1,28 +1,19 @@
+// Option to disable include resolution inside the CI when we only want to run preprocessor steps.
+#ifndef HASS_SENSOR_SKIP_INCLUDES
+
 #pragma once
 
-#include "soc/gpio_num.h"
+#include <cstdint>
+#include <soc/gpio_num.h>
 
-/**
- * Compile the application for the seed studio XIAO ESPC6.
- * Docs: https://wiki.seeedstudio.com/xiao_esp32c6_getting_started/
- **/
-#define HASS_SENSOR_DEVICE_SEED_STUDIO_XIAO_ESPC6
+#endif // !HASS_SENSOR_SKIP_INCLUDES
 
-/**
- * Compile the application for the official ESP32 H2 developer kit.
- * Docs: https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32h2/index.html
- **/
-// #define HASS_SENSOR_DEVICE_H2_DEVKIT
-
-/**
- * Compile the application for the official ESP32 C6 developer kit.
- * Docs: https://docs.espressif.com/projects/esp-dev-kits/en/latest/esp32c6/index.html
- **/
-// #define HASS_SENSOR_DEVICE_C6_DEVKIT
-
-#define CONFIG_HASS_ENVIRONMENT_SENSOR_SCD41_MOCK
 // ------------------------------------------------------------
-#ifdef HASS_SENSOR_DEVICE_SEED_STUDIO_XIAO_ESPC6
+#ifdef CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_SEED_STUDIO_XIAO_ESPC6
+
+// OTA image type to correctly identify which image is for which device.
+constexpr uint16_t OTA_IMAGE_TYPE = 1;
+
 // Use the internal or external antenna
 constexpr bool HASS_SENSOR_ANTENNA_EXTERNAL = true;
 
@@ -35,14 +26,14 @@ constexpr gpio_num_t HASS_SENSOR_SCD4X_SCL_GPIO = gpio_num_t::GPIO_NUM_23;
 constexpr gpio_num_t HASS_SENSOR_STATUS_LED_GPIO = gpio_num_t::GPIO_NUM_15;
 constexpr bool HASS_SENSOR_STATUS_LED_LOW_ACTIVE = true;
 
-// Validation
-#if defined(HASS_SENSOR_DEVICE_H2_DEVKIT) || defined(HASS_SENSOR_DEVICE_C6_DEVKIT)
-#error "Only a single hardware target can be defined!"
-#endif
-#endif // HASS_SENSOR_DEVICE_SEED_STUDIO_XIAO_ESPC6
+#endif // CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_SEED_STUDIO_XIAO_ESPC6
 
 // ------------------------------------------------------------
-#ifdef HASS_SENSOR_DEVICE_H2_DEVKIT
+#ifdef CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_ESP32_H2_DEV_KIT
+
+// OTA image type to correctly identify which image is for which device.
+constexpr uint16_t OTA_IMAGE_TYPE = 2;
+
 // SCD4X I2C GPIOs
 constexpr gpio_num_t HASS_SENSOR_SCD4X_SDA_GPIO = gpio_num_t::GPIO_NUM_12;
 constexpr gpio_num_t HASS_SENSOR_SCD4X_SCL_GPIO = gpio_num_t::GPIO_NUM_22;
@@ -51,14 +42,14 @@ constexpr gpio_num_t HASS_SENSOR_SCD4X_SCL_GPIO = gpio_num_t::GPIO_NUM_22;
 #define HASS_SENSOR_DEBUG_RGB_LED_ENABLED
 constexpr gpio_num_t HASS_SENSOR_DEBUG_RGB_LED_GPIO = gpio_num_t::GPIO_NUM_8;
 
-// Validation
-#if defined(HASS_SENSOR_DEVICE_SEED_STUDIO_XIAO_ESPC6) || defined(HASS_SENSOR_DEVICE_C6_DEVKIT)
-#error "Only a single hardware target can be defined!"
-#endif
-#endif // HASS_SENSOR_DEVICE_H2_DEVKIT
+#endif // CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_ESP32_H2_DEV_KIT
 
 // ------------------------------------------------------------
-#ifdef HASS_SENSOR_DEVICE_C6_DEVKIT
+#ifdef CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_ESP32_C6_DEV_KIT
+
+// OTA image type to correctly identify which image is for which device.
+constexpr uint16_t OTA_IMAGE_TYPE = 3;
+
 // SCD4X I2C GPIOs
 constexpr gpio_num_t HASS_SENSOR_SCD4X_SDA_GPIO = gpio_num_t::GPIO_NUM_12;
 constexpr gpio_num_t HASS_SENSOR_SCD4X_SCL_GPIO = gpio_num_t::GPIO_NUM_22;
@@ -67,8 +58,4 @@ constexpr gpio_num_t HASS_SENSOR_SCD4X_SCL_GPIO = gpio_num_t::GPIO_NUM_22;
 #define HASS_SENSOR_DEBUG_RGB_LED_ENABLED
 constexpr gpio_num_t HASS_SENSOR_DEBUG_RGB_LED_GPIO = gpio_num_t::GPIO_NUM_8;
 
-// Validation
-#if defined(HASS_SENSOR_DEVICE_H2_DEVKIT) || defined(HASS_SENSOR_DEVICE_SEED_STUDIO_XIAO_ESPC6)
-#error "Only a single hardware target can be defined!"
-#endif
-#endif // HASS_SENSOR_DEVICE_C6_DEVKIT
+#endif // CONFIG_HASS_ENVIRONMENT_SENSOR_DEVICE_TARGET_ESP32_C6_DEV_KIT
