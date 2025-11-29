55c8bc91dfbafcd0610701504532b9bcce9047a1
Fabian Sauter <sauter.fabian@mailbox.org>
2025-11-29 09:23:08 +0100

    Reduced log level to debug

--- firmware/esp32/main/HassSensorMain.cpp
+++ firmware/esp32/main/HassSensorMain.cpp
@@ -31,7 +31,7 @@ const char* TAG = "hassSensor";
 } // namespace
 
 void mainLoop() {
-    esp_log_level_set(TAG, ESP_LOG_VERBOSE);
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
     ESP_LOGI(TAG, "Starting HASS environment sensor version %d.%d.%d", CONFIG_HASS_ENVIRONMENT_SENSOR_VERSION_MAJOR, CONFIG_HASS_ENVIRONMENT_SENSOR_VERSION_MINOR, CONFIG_HASS_ENVIRONMENT_SENSOR_VERSION_PATCH);
 
